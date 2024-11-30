20cc678936d2f837e94fcec3f3901d559f2c434b
anurag1302 <anurag3487@gmail.com>
2024-11-30 18:40:49 +0530

    callbacks implementation for greet

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log("index works");
 
-function loadScript(src) {
+function loadScript(src, callback) {
   let script = document.createElement("script");
   script.src = src;
+  script.onload = function () {
+    callback();
+  };
   document.head.append(script);
 }
 
-loadScript("scripts.js");
-
-greet();
+loadScript("scripts.js", function () {
+  greet();
+});
 
 console.log("I am called after scripts.js is appended dynamically");
