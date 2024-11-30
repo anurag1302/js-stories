e344a3faa4a3fb0f1bba00b6e1e11b0abe4d4190
anurag1302 <anurag3487@gmail.com>
2024-11-30 19:36:35 +0530

    Promisify the callback

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 console.log("index works");
 
-function loadScript(src, callback) {
-  let script = document.createElement("script");
-  script.src = src;
-  script.onload = function () {
-    callback();
-  };
-  document.head.append(script);
+//Using callbacks
+// function loadScript(src, callback) {
+//   let script = document.createElement("script");
+//   script.src = src;
+//   script.onload = function () {
+//     callback();
+//   };
+//   document.head.append(script);
+// }
+
+// loadScript("scripts.js", function () {
+//   greet();
+// });
+
+// console.log("I am called after scripts.js is appended dynamically");
+
+/**************************/
+//Using Promises
+
+function loadScript2(src) {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.src = src;
+    script.onload = function () {
+      resolve(script);
+    };
+    script.onerror = function () {
+      reject(new Error(`Something went wrong in ${src} invocation`));
+    };
+    document.head.append(script);
+  });
 }
 
-loadScript("scripts.js", function () {
-  greet();
-});
+let promise = loadScript2("scripts.js");
+console.log(promise);
 
-console.log("I am called after scripts.js is appended dynamically");
+promise
+  .then((res) => {
+    console.log(res);
+    greet();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
